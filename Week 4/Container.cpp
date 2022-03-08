eee0a1d051774433a6b5e308565102c4e293c529
chang_m_h <82326301+chmh0624@users.noreply.github.com>
2022-03-08 14:15:49 +0800

    Create Container.cpp

---
+++ Week 4/Container.cpp
@@ -0,0 +1,11 @@
+class container
+{
+public:
+  container(void): box(NULL), len(0), index(0){}
+  int getLen() {return len;}
+  int getIndex() {return index;}
+private:
+  int *box;
+  int len;
+  int index;
+};
