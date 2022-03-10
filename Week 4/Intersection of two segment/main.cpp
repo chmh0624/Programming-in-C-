3661b839c504efbef22fa86dc6a43900d1690f80
chang_m_h <82326301+chmh0624@users.noreply.github.com>
2022-03-10 08:48:02 +0800

    Create main.cpp

---
+++ Week 4/Intersection of two segment/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cmath>
+
+class Coordinate
+{
+public:
+  Coordinate()
+  {
+    x = 0;
+    y = 0;
+  }
+  Coordinate(int _x, int _y)
+  {
+    x = _x;
+    y = _y;
+  }
+  Coordinate(const Coordinate &obj)
+  {
+    x = obj.x;
+    y = obj.y;
+  }
+  int getX()
+  {
+    return x;
+  }
+  int getY()
+  {
+    return y;
+  }
+  void setX(int _x)
+  {
+    x = _x;
+  }
+  void setY(int _y)
+  {
+    y = _y;
+  }
+  void showCoordInfo()
+  {
+    std::cout << "(" << x << ", " << y << ")\n";
+  }
+
+private:
+  int x, y;
+};
+
+class Segment // using two Coordinate to represent a segment
+{
+
+public:
+  Segment(Coordinate _a, Coordinate _b)
+  {
+    // constructor
+  }
+  
+  Coordinate *getIntersection(Segment S)
+  {
+    // code here
+  }
+
+  float length() 
+  {
+    // return the length of segment
+  }
+
+  Coordinate getCoordinateA() { return a; }
+
+  Coordinate getCoordinateB() { return b; }
+
+private:
+  Coordinate a, b;
+};
+
+
+int main()
+{
+  // change the coordinate
+  Coordinate a = Coordinate(0,0);
+  Coordinate b = Coordinate(1,0);
+  Coordinate c = Coordinate(1,1);
+  Coordinate d = Coordinate(-1,-1);
+  Segment s1 = Segment(a, b);
+  Segment s2 = Segment(c, d);
+  (s1.getIntersection(s2)) -> showCoordInfo();
+}
