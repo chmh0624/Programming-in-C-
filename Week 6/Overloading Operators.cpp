1b6aa2ccf3320073c137b587b4ae0fc5bc202b51
chang_m_h <82326301+chmh0624@users.noreply.github.com>
2022-03-25 21:32:18 +0800

    Create Overloading Operators.cpp

---
+++ Week 6/Overloading Operators.cpp
@@ -0,0 +1,27 @@
+class Coordinate
+{
+public:
+  Coordinate(){}
+  Coordinate(const Coordinate &c) {x = c.x; y = c.y; }
+  Coordinate(int _x, int _y) {x = _x; y = _y; }
+  int getX() const { return x; }
+  int getY() const { return y; }
+  void setX(int val) { x = val; }
+  void setY(int val) { y = val; }
+  void showCoordInfo() const { std::cout << "(" << x << ", " << y << ")\n"; }
+  int operator > (Coordinate &b){ return x > b.x & y > b.y; }
+  int operator < (Coordinate &b){ return x < b.x & y < b.y; }
+  Coordinate operator + (Coordinate &b)
+  {
+    Coordinate *c = new Coordinate(x + b.x, y + b.y);
+    return *c;
+  }
+  Coordinate operator - (Coordinate &b)
+  {
+    Coordinate *c = new Coordinate(x - b.x, y - b.y);
+    return *c;
+  }
+  
+private:
+  int x, y;
+};
