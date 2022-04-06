ab54b9d7ced33634b222539c89ab06ac30aa7088
chang_m_h <82326301+chmh0624@users.noreply.github.com>
2022-04-06 10:48:30 +0800

    Create Accept.cpp

---
+++ Week 7/Geometric Progression/Accept.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+class F{
+private:
+	double a, r;
+public:
+	F(double _a, double _r) { a = _a; r = _r; }
+	double at(int x) { return a * pow(r, x); }
+	double *S()
+	{
+		if (r >= 1 || r <= -1)
+			return nullptr;
+		double *n = new double;
+		*n = a / (1 - r);
+		return n;
+	}
+};
+
+int main()
+{
+	int j, k;
+	double a, r;
+	std::cin >> a >> r;
+	F f(a, r);
+	for(k = 0; k < 3; k ++)
+		printf("%.2lf\n", f.at(k));
+	double *s = f.S();
+    if(s == NULL)
+      printf("NULL\n");
+    else
+      printf("%.2lf\n", *s);
+}
