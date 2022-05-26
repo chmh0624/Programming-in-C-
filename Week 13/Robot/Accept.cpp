ac0a0f1da1d6001ab483e7ef609c58b1fc6dadeb
chang_m_h <82326301+chmh0624@users.noreply.github.com>
2022-05-26 16:02:37 +0800

    Update Accept.cpp

--- Week 13/Robot/Accept.cpp
+++ Week 13/Robot/Accept.cpp
@@ -1,72 +1,73 @@
 int main()
 {
-    std::string ins, tmp;
-    Robot robot;
-    while (std::cin >> ins)
-    {
-        if (ins == "GO")
-        {
-            int n;
-            std::cin >> n;
-            while (n--)
-                robot.go();
-            std::cin >> ins;
-        }
-        else if (ins == "TURN")
-        {
-            std::string way;
-            std::cin >> way;
-            robot.turn(way);
-        }
-        else if (ins == "TURNTO")
-        {
-            char dir;
-            std::cin >> dir;
-            while (dir != robot.getDirection())
-                robot.turn("RIGHT");
-        }
-        else if (ins == "GOTO")
-        {
-            int x, y;
-            std::cin >> x >> y;
-            if (x > robot.getPositionX())
-            {
-                while ('E' != robot.getDirection())
-                    robot.turn("RIGHT");
-                while (x > robot.getPositionX())
-                    robot.go();
-            }
-            else if (x < robot.getPositionX())
-            {
-                while ('W' != robot.getDirection())
-                    robot.turn("RIGHT");
-                while (x > robot.getPositionX())
-                    robot.go();
-            }
-            if (y > robot.getPositionY())
-            {
-                while ('N' != robot.getDirection())
-                    robot.turn("RIGHT");
-                while (y > robot.getPositionY())
-                    robot.go();
-            }
-            else if (y < robot.getPositionY())
-            {
-                while ('S' != robot.getDirection())
-                    robot.turn("RIGHT");
-                while (y < robot.getPositionY())
-                    robot.go();
-            }
-            while ('N' != robot.getDirection())
-                robot.turn("RIGHT");
-        }
-        else if (ins == "SHOWINFO")
-        {
-            robot.display();
-        }
-        else
-        {
-            break;
-        }
-    }
+	std::string ins;
+	Robot robot;
+	while (std::cin >> ins)
+	{
+		if (ins == "GO")
+		{
+			int n;
+			std::cin >> n;
+			while (n--)
+				robot.go();
+          	std::cin >> ins;
+		}
+		else if (ins == "TURN")
+		{
+			std::string way;
+			std::cin >> way;
+			robot.turn(way);
+		}
+		else if (ins == "TURNTO")
+		{
+			char dir;
+			std::cin >> dir;
+			while (dir != robot.getDirection())
+				robot.turn("RIGHT");
+		}
+		else if (ins == "GOTO")
+		{
+			int x, y;
+			std::cin >> x >> y;
+			if (x > robot.getPositionX())
+			{
+				while ('E' != robot.getDirection())
+					robot.turn("RIGHT");
+				while (x > robot.getPositionX())
+					robot.go();
+			}
+			else if (x < robot.getPositionX())
+			{
+				while ('W' != robot.getDirection())
+					robot.turn("RIGHT");
+				while (x < robot.getPositionX())
+					robot.go();
+			}
+			if (y > robot.getPositionY())
+			{
+				while ('N' != robot.getDirection())
+					robot.turn("RIGHT");
+				while (y > robot.getPositionY())
+					robot.go();
+			}
+			else if (y < robot.getPositionY())
+			{
+				while ('S' != robot.getDirection())
+					robot.turn("RIGHT");
+				while (y < robot.getPositionY())
+					robot.go();
+			}
+          	while ('N' != robot.getDirection())
+				robot.turn("RIGHT");
+		}
+		else if (ins == "SHOWINFO")
+		{
+			robot.display();
+		}
+		else
+		{
+			break;
+		}
+
+	}
 }
